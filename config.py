7258d66c35011c37186e55c0908cdb6b29caaefb
Dmitrieva <92915335+ferradeth@users.noreply.github.com>
2022-10-12 17:44:38 +0500

    del tokens

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-token = "5609022316:AAHXdL1VNBqgFYy0Pz810G7irSZ5k2DoHnk"
-news_key = '9fa2328382ec484e9ea27f417076ba30'
+token = "enter your bot token"
+news_key = 'enter your api key'
 categories = ['business', 'entertainment', 'general', 'health', 'science', 'sports', 'technology']
 url = 'http://127.0.0.1:5000'
