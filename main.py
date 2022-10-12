2b7974e9ef6f2b5642634ac8027f214daf9b6e8d
feradeth <gankachan130903@gmail.com>
2022-10-12 17:43:04 +0500

    finish version

--- main.py
+++ main.py
@@ -2,23 +2,8 @@ import telebot
 from telebot import types
 import config
 import requests
-import sqlite3
-
-connect = sqlite3.connect('mydb.db', check_same_thread=False)
-
-cursor = connect.cursor()
-cursor.execute('''CREATE TABLE IF NOT EXISTS "users"(id INTEGER PRIMARY KEY, login TEXT NOT NULL); ''')
-cursor.execute('''CREATE TABLE IF NOT EXISTS "categories"(id INTEGER PRIMARY KEY, name TEXT NOT NULL); ''')
-cursor.execute('''CREATE TABLE IF NOT EXISTS "subs"(user_id INTEGER NOT NULL, category_id INTEGER NOT NULL); ''')
-
-connect.commit()
 
 bot = telebot.TeleBot(config.token, parse_mode=None)
-
-if len(cursor.execute('SELECT id, name FROM categories').fetchall()) < len(config.categories):
-    for cat in config.categories:
-        cursor.execute('INSERT INTO "categories"(name) VALUES (?)', (cat,))
-        connect.commit()
 
 
 @bot.message_handler(commands=['start'])
@@ -29,102 +14,79 @@ def send_welcome(message, ):
     }
     requests.post(config.url+'/start', json=data)
     bot.send_message(message.from_user.id, "привет, рад тебя видеть <3")
-    # login = message.from_user.id
-    # print(login)
-    # if login != '':
-    #     result = cursor.execute('SELECT login FROM users WHERE login=?', (login,)).fetchone()
-    #     if result is None:
-    #         cursor.execute('INSERT INTO "users"(login) VALUES (?)', (login,))
-    #         connect.commit()
 
 
 @bot.message_handler(commands=['help'])
 def help_txt(message):
-    bot.send_message(message.from_user.id, "/start - начать работу \n /sub - подписаться на категорию \n мои подписки "
-                                           "- посмотреть подписки \n /unsub - отписаться от категории \n /news - "
-                                           "показать новости")
+    bot.send_message(message.from_user.id, "/start - начать работу \n /sub - подписаться на категорию \n /unsub - "
+                                           "отписаться от категории \n /news - показать новости")
 
 
 @bot.message_handler(commands=['news'])
 def send_news(message):
-    signup = cursor.execute('SELECT id FROM users WHERE login=?', (message.from_user.id,)).fetchone()
-    user_cats = cursor.execute('SELECT category_id FROM subs WHERE user_id=?', (signup[0],)).fetchall()
-    if user_cats is not None:
-        for u_cat in user_cats:
-            category = cursor.execute('SELECT name FROM categories WHERE id=?', (u_cat[0],)).fetchone()
-            a = requests.get(f'https://newsapi.org/v2/top-headlines?apiKey={config.news_key}&country=ru&pageSize=2'
-                             f'&category={category[0]}')
-            for i in a.json()['articles']:
-                news = [[i['title'], i['publishedAt'], i['url']]]
-                for new in news:
-                    bot.send_message(message.from_user.id, f" {new[0]} {new[2]}")
+    login = message.from_user.id
+    url = config.url + "/news"
+
+    data = {
+        "login": login
+    }
+
+    sub_categories = requests.post(url, json=data)
+
+    for category in sub_categories.json():
+        req = requests.get(
+            f'https://newsapi.org/v2/top-headlines?apiKey={config.news_key}&country=cn&pageSize=3&category={category["name"]}')
+        for i in req.json()["articles"]:
+            news_list = [[i["title"], i["publishedAt"], i["url"]]]
+            print(news_list)
+            for new in news_list:
+                bot.send_message(message.from_user.id, f"{new[0]} {new[2]}")
 
 
 @bot.message_handler(commands=['sub'])
 def sub_news(message):
     cats = requests.post(config.url+'/sub/info')
-    print(cats)
     markup = types.ReplyKeyboardMarkup()
     if cats is not None:
-        for category in cats:
-            markup.add(types.KeyboardButton(category[1]))
+        for category in cats.json():
+            markup.add(types.KeyboardButton(f"{category['name']}"))
     bot.send_message(message.from_user.id, 'Выберете категорию для подписки: ', reply_markup=markup)
     bot.register_next_step_handler(message, add_sub)
 
 
 def add_sub(message):
-    result = cursor.execute('SELECT id FROM categories WHERE name=?', (message.text,)).fetchone()
-    if result is not None:
-        signup = cursor.execute('SELECT id FROM users WHERE login=?', (message.from_user.id,)).fetchone()
-        check_ex = cursor.execute('SELECT * FROM subs WHERE user_id=? AND category_id=?', (message.from_user.id, result[0])).fetchone()
-        if check_ex is None:
-            cursor.execute('INSERT INTO subs(user_id, category_id) VALUES (?, ?)', (signup[0], result[0]))
-            bot.send_message(message.from_user.id, 'Вы успешно подписались на категорию')
-            connect.commit()
-        else:
-            bot.send_message(message.from_user.id, 'Вы уже подписаны на эту категорию')
-    else:
-        bot.send_message(message.from_user.id,'Проверьте корректность ввода, возможно такой категории не существует')
+    login = message.from_user.id
+    data = {
+        "login": login,
+        "category": message.text
+    }
+
+    result = requests.post(config.url+"/sub/sub", json=data)
+    bot.send_message(message.from_user.id, result.json()['answer'])
 
 
 @bot.message_handler(commands=['unsub'])
 def unsub_cat(message):
-    signup = cursor.execute('SELECT id FROM users WHERE login=?', (message.from_user.id,)).fetchone()
-    user_cats = cursor.execute('SELECT category_id FROM subs WHERE user_id=?', (signup[0],)).fetchall()
+    data = {
+        "login": message.from_user.id
+    }
+    result = requests.post(config.url+"/unsub/cats", json=data)
+    print(result.json())
     markup = types.ReplyKeyboardMarkup()
-    if user_cats is not None:
-        for u_cat in user_cats:
-            category = cursor.execute('SELECT name FROM categories WHERE id=?', (u_cat[0],)).fetchone()
-            markup.add(types.KeyboardButton(category[0]))
+    if result is not None:
+        for u_cat in result.json():
+            markup.add(types.KeyboardButton(u_cat['name']))
     bot.send_message(message.from_user.id, 'От какой категории вы хотите отписаться? ', reply_markup=markup)
     bot.register_next_step_handler(message, del_sub)
 
 
 def del_sub(message):
-    signup = cursor.execute('SELECT id FROM users WHERE login=?', (message.from_user.id,)).fetchone()
-    result = cursor.execute('SELECT id FROM categories WHERE name=?', (message.text,)).fetchone()
-    if result is not None:
-        cursor.execute('DELETE FROM subs WHERE category_id=? AND user_id=?', (result[0], signup[0]))
-        bot.send_message(message.from_user.id, 'Вы успешно отписались от категории')
-        connect.commit()
-    else:
-        bot.send_message(message.from_user.id, 'Введите корректное название категории')
-
-
-@bot.message_handler(func=lambda m: True)
-def say_smt(message):
-    if message.text == 'лох':
-        bot.send_message(message.from_user.id, 'наелся блох сел на лавочку и сдох')
-    elif message.text == 'расскажи шутку':
-        bot.send_message(message.from_user.id,
-                         'Штирлиц упал с четырнадцатого этажа. Так широко раскидывать мозгами ему еще не приходилось.')
-    elif message.text == 'мои подписки':
-        signup = cursor.execute('SELECT id FROM users WHERE login=?', (message.from_user.id,)).fetchone()
-        user_cats = cursor.execute('SELECT category_id FROM subs WHERE user_id=?', (signup[0],)).fetchall()
-        if user_cats is not None:
-            for u_cat in user_cats:
-                bot.send_message(message.from_user.id,
-                                 cursor.execute('SELECT name FROM categories WHERE id=?', (u_cat[0],)).fetchone()[0])
+    data = {
+        "login": message.from_user.id,
+        "name":  message.text
+    }
+    result = requests.post(config.url + "/unsub/del", json=data)
+    bot.send_message(message.from_user.id, result.json()['answer'])
 
 
 bot.infinity_polling()
--- server.py
+++ server.py
@@ -1,4 +1,5 @@
-from flask import Flask, request
+from dataclasses import dataclass
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 import config
 
@@ -7,34 +8,37 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 database = SQLAlchemy(app)
 
 
+@dataclass
 class Users(database.Model):
+    id: int
+    login: str
+
     id = database.Column(database.Integer, primary_key=True)
     login = database.Column(database.String(64))
 
-    def __repr__(self):
-        return "<Users %r>" % self.id
 
-
+@dataclass
 class Categories(database.Model):
+    id: int
+    name: str
+
     id = database.Column(database.Integer, primary_key=True)
     name = database.Column(database.String(64))
 
-    def __repr__(self):
-        return "<Categories %r>" % self.id
 
-
+@dataclass
 class Subs(database.Model):
+    user_id: int
+    category_id: int
+
     user_id = database.Column(database.Integer, primary_key=True)
-    category_id = database.Column(database.String(64))
-
-    def __repr__(self):
-        return "<Categories %r>" % self.id
+    category_id = database.Column(database.Integer, primary_key=True)
 
 
-# if len(database.session.execute(database.select(Categories)).scalars()) < len(config.categories):
-#     for cat in config.categories:
-#         database.session.add(Categories(name=cat))
-#         database.session.commit()
+if len(database.session.execute(database.select(Categories)).all()) < len(config.categories):
+    for cat in config.categories:
+        database.session.add(Categories(name=cat))
+        database.session.commit()
 
 
 @app.route("/start", methods=["POST", "GET"])
@@ -42,18 +46,82 @@ def reg_user():
     if request.method == 'POST':
         print(request.json['login'])
         # print(database.session.execute(database.select(Users).filter_by(login=request.json['login'])).one())
-        if database.session.execute(database.select(Users).filter_by(login=request.json['login'])).first() is None:
+        if Users.query.filter_by(login=request.json['login']).first() is None:
             database.session.add(Users(login=request.json['login']))
             database.session.commit()
             print('пользователь зарегистрировался')
     return {'ok': True}
 
 
-# @app.route("/sub/info", methods=["POST", "GET"])
-# def add_sub():
-#     if request.method == 'POST':
-#         return database.session.execute(database.select(Categories)).scalars()
+@app.route("/sub/info", methods=["POST", "GET"])
+def add_sub():
+    if request.method == 'POST':
+        categories = Categories.query.all()
+        return jsonify(categories)
 
+
+@app.route("/sub/sub", methods=["POST", "GET"])
+def sub_sub():
+    if request.method == 'POST':
+        res_cat = database.session.execute(database.select(Categories.id).filter_by(name=request.json['category'])).first()
+        print(res_cat)
+        if res_cat is not None:
+            signup = database.session.execute(database.select(Users.id).filter_by(login=request.json['login'])).first()
+            print(signup)
+            check_ex = Subs.query.filter_by(user_id=signup[0], category_id=res_cat[0]).first()
+            if check_ex is None:
+                database.session.add(Subs(user_id=signup[0], category_id=res_cat[0]))
+                result = {'answer': 'Вы успешно подписались на категорию'}
+                database.session.commit()
+            else:
+                result = {'answer': 'Вы уже подписаны на эту категорию'}
+        else:
+            result = {'answer': 'Проверьте ввод, возможно такой категории не существует'}
+        return result
+
+
+@app.route("/unsub/cats", methods=["POST", "GET"])
+def info_subs():
+    if request.method == "POST":
+        signup = database.session.execute(database.select(Users.id).filter_by(login=request.json['login'])).first()
+        user_cats = database.session.execute(database.select(Subs.category_id).filter_by(user_id=signup[0])).all()
+        print(user_cats)
+        subs = []
+        for s_id in user_cats:
+            category = Categories.query.filter_by(id=s_id[0]).first()
+            subs.append(category)
+        print(jsonify(subs))
+        return jsonify(subs)
+
+
+@app.route("/unsub/del", methods=["POST", "GET"])
+def del_sub():
+    result = {}
+    if request.method == "POST":
+        signup = database.session.execute(database.select(Users.id).filter_by(login=request.json['login'])).first()
+        print(f"айди пользователя: {signup[0]}")
+        cat_sub = database.session.execute(database.select(Categories.id).filter_by(name=request.json['name'])).first()
+        print(f"айди категории: {cat_sub[0]}")
+        if cat_sub is not None:
+            Subs.query.filter_by(user_id=signup[0], category_id=cat_sub[0]).delete(synchronize_session=False)
+            database.session.commit()
+            result = {'answer': 'Вы успешно отписались от категории'}
+        else:
+            result = {'answer': 'Вы не подписаны на эту категорию'}
+    return result
+
+
+@app.route("/news", methods=["POST", "GET"])
+def send_news():
+    user_id = database.session.execute(database.select(Users.id).filter_by(login=request.json["login"])).first()
+    categories = database.session.execute(database.select(Subs.category_id).filter_by(user_id=user_id[0])).all()
+    user_subs = []
+
+    for category in categories:
+        sub_categories = Categories.query.filter_by(id=category[0]).first()
+        user_subs.append(sub_categories)
+
+    return jsonify(user_subs)
 
 if __name__ == "__main__":
     app.run()
